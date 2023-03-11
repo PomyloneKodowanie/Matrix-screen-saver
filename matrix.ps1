d932df9dc73793550f587ecdcf40ccf5b025514e
PomyloneKodowanie <PomyloneKodowanie@gmail.com>
2023-03-11 17:26:30 +0100

    working as intended, added color change

--- matrix.ps1
+++ matrix.ps1
@@ -1,30 +1,27 @@
 #Hello World ! :)
+#Set-ExecutionPolicy -Scope CurrentUser Unrestricted
 $width = ($Host.UI.RawUI.WindowSize.Width);
 $height = ($Host.UI.RawUI.WindowSize.Height);
-#cat /dev/urandom | tr -dc '0-9' | fold -w 211
-# while true; do
-# 	screen=$(timeout 0.06s cat /dev/urandom | tr -dc '0-9' | fold -w 211 | head -1)
-# 	echo $screen
-# 	sleep 1s
-# done
-$screen = " ";
-$i = 0;
-$ih = 0;
-while (1)
+
+# save the current color // seems unenncesary in current version of PS
+#$fc = $host.UI.RawUI.ForegroundColor;
+#$bc = $Host.UI.RawUI.BackgroundColor;
+
+#Sets UI colors
+$Host.UI.RawUI.ForegroundColor = "Green";
+$Host.UI.RawUI.BackgroundColor = "Black";
+$ih = 0; #iterator for height
+while (1) #Loop to infinity, while restarting the loop when screen height is reached, (does it serve anything, I don't know)
 {
-    while ($ih -le $height)
+    while ($ih -le $height) #Print one line at the time
     {
-        while (($screen.Length +1) -le $width)
+        while (($screen.Length +1) -le $width) # Crate a console wide string ($screen) through adding random numbers and white spaces until the console width is filled, the (+1) is usede for padding, the classical < less than would probabaly work but i don't know how to do it ATM.
         {
            $screen = -join($screen, " ", (Get-Random 10));
-             $i++;
         }
-    echo $screen;
-    $i = 0;
+    Write-Output $screen;
     $screen = "";
     $ih++;
-
-
     }
 $screen = "";
 $ih = 0;
